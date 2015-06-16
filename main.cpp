08897b44cbfca089e12115b7c0a98530dd486510
Martin Wickham <spexguy070@gmail.com>
2015-06-16 15:45:04 -0500

    [DNB] FollowingZFill nitpicking and cleanup

--- main.cpp
+++ main.cpp
@@ -37,43 +37,21 @@ class FollowingZFill : public ZFill {
 public:
     virtual void OnIntersection(IntPoint& e1bot, IntPoint& e1top, bool e1Forward,
                                 IntPoint& e2bot, IntPoint& e2top, bool e2Forward,
-                                const IntPoint &pt, cInt& z1f, cInt& z1r, cInt& z2f, cInt& z2r) {
+                                const IntPoint &pt, cInt& z1f, cInt& z1r, cInt& z2f, cInt& z2r) override {
         // I was really hoping that there was some great gestalt of understanding for this.
         // But I can't see it. This routine is the result of enumerating all 4 cases.
 
-        // The problem decomposes nicely into independent problems on each edge.
+        // The problem decomposes nicely into one problem repeated independently on each edge.
         // a     d
         //  \   /
         // z1f z2f
         // z1r z2r
         //  /   \
         // c     f
-
-        if (e1Forward) {
-            // set z1f=b(f), z2r=r(e(f)), e1top.Z=e(f)
-            z1f = StripBegin(e1top.Z, e1bot, e1top, pt);
-            z2r = ReverseZ(StripEnd(e1top.Z, e1bot, e1top, pt));
-            e1top.Z = StripEnd(e1top.Z, e1bot, e1top, pt);
-        } else {
-            // set z1f=r(e(a)), z2r=b(a), e1bot.Z=b(a)
-            z1f = ReverseZ(StripEnd(e1bot.Z, e1top, e1bot, pt));
-            z2r = StripBegin(e1bot.Z, e1top, e1bot, pt);
-            e1bot.Z = StripBegin(e1bot.Z, e1top, e1bot, pt);
-        }
-
-        if (e2Forward) {
-            // set z2f=b(c), z1r=r(e(c)), e2top.Z=e(c)
-            z2f = StripBegin(e2top.Z, e2bot, e2top, pt);
-            z1r = ReverseZ(StripEnd(e2top.Z, e2bot, e2top, pt));
-            e2top.Z = StripEnd(e2top.Z, e2bot, e2top, pt);
-        } else {
-            // set z2f=r(e(d)), z1r=b(d), e2bot.Z=e(d)
-            z2f = ReverseZ(StripEnd(e2bot.Z, e2top, e2bot, pt));
-            z1r = StripBegin(e2bot.Z, e2top, e2bot, pt);
-            e2bot.Z = StripBegin(e2bot.Z, e2top, e2bot, pt);
-        }
+        SplitEdge(e1bot, e1top, e1Forward, pt, z1f, z2r);
+        SplitEdge(e2bot, e2top, e2Forward, pt, z2f, z1r);
     }
-    virtual void OnPoint(IntPoint& prev, IntPoint& curr, IntPoint& next, bool forward, IntPoint2Z& pt) {
+    virtual void OnPoint(IntPoint& prev, IntPoint& curr, IntPoint& next, bool forward, IntPoint2Z& pt) override {
         if (forward) {
             pt.forwardZ = curr.Z;
             pt.reverseZ = ReverseZ(next.Z);
@@ -83,7 +61,7 @@ public:
         }
     }
 
-    virtual void BeginLoopReversal(IntPoint& last, IntPoint& first, cInt filler) {
+    virtual void BeginLoopReversal(IntPoint& last, IntPoint& first, cInt filler) override {
         assert(firstWas == 0);
         firstWas = first.Z;
         if (filler) {
@@ -92,23 +70,38 @@ public:
             first.Z = ReverseZ(last.Z);
         }
     }
-    virtual void ReverseEdge(IntPoint& first, IntPoint& second) {
+    virtual void ReverseEdge(IntPoint& first, IntPoint& second) override {
         cInt firstWill = second.Z;
         second.Z = ReverseZ(firstWas);
         firstWas = firstWill;
     }
-    virtual void FinishLoopReversal(IntPoint& last, IntPoint& first) {
+    virtual void FinishLoopReversal(IntPoint& last, IntPoint& first) override {
         firstWas = 0;
     }
 
-    virtual void OnOffset(int step, int steps, IntPoint& source, IntPoint& dest) {
+    virtual void OnOffset(int step, int steps, IntPoint& source, IntPoint& dest) override {
         dest.Z = Clone(source.Z);
     }
 
 protected:
     virtual cInt ReverseZ(cInt z) {return z;}
     virtual cInt Clone(cInt z) {return z;}
-    virtual cInt StripBegin(cInt& z, IntPoint& from, IntPoint& to, const IntPoint& pt) {return z;}
-    virtual cInt StripEnd(cInt& z, IntPoint& from, IntPoint& to, const IntPoint& pt) {return z;}
+    virtual cInt StripBegin(cInt z, IntPoint& from, IntPoint& to, const IntPoint& pt) {return z;}
+    virtual cInt StripEnd(cInt z, IntPoint& from, IntPoint& to, const IntPoint& pt) {return z;}
     cInt firstWas = 0;
+
+private:
+    void SplitEdge(IntPoint& bot, IntPoint& top, bool forward, const IntPoint& pt, cInt& zf, cInt& zr) {
+        if (forward) {
+            // set z1f=b(f), z2r=r(e(f)), e1top.Z=e(f)
+            zf = StripBegin(top.Z, bot, top, pt);
+            zr = ReverseZ(StripEnd(top.Z, bot, top, pt));
+            top.Z = StripEnd(top.Z, bot, top, pt);
+        } else {
+            // set z1f=r(e(a)), z2r=b(a), e1bot.Z=b(a)
+            zf = ReverseZ(StripEnd(bot.Z, top, bot, pt));
+            zr = StripBegin(bot.Z, top, bot, pt);
+            bot.Z = StripBegin(bot.Z, top, bot, pt);
+        }
+    }
 };
